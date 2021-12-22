b54c44b21d3b15ad91adf40d0c630e69f2db0d4b
Daniel Madar <m.a.daniel@gmail.com>
2021-12-22 11:13:09 +0200

    draft

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "<h1>Main</h1>"
+
+@app.route('/awards')
+def awards():    
+    return render_template("awards.html")
+    
+if __name__ == '__main__':  
+    app.run(host='127.0.0.1', port=80, debug=True)
+
