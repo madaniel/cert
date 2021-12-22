42621e893c0ac818e77fdfc266c006983d46386f
Daniel Madar <m.a.daniel@gmail.com>
2021-12-22 11:35:43 +0200

    fixing files for Heroku

--- app/main.py
+++ app/main.py
@@ -5,12 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "<h2>Main</h2>"
+    return "<h2>Welcome to Daniel Madar Certification Web App</h2>"
+
 
 @app.route('/awards')
 def awards():    
     return render_template("awards.html")
-    
-if __name__ == '__main__':  
-    app.run(host='127.0.0.1', port=80, debug=True)
-
